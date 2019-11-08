ea41f1932ba235102a1b5ab2e9e01bc395d47450
Tobias Kiecker <kieckert@informatik.hu-berlin.de>
2019-11-08 13:35:51 +0000

    kommentar hinzugefügt (TEST)

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+], function (Controller) {  //test
 	"use strict";
 
 	return Controller.extend("Mobilitaetskonto.Mobilitaetskonto.controller.View1", {
