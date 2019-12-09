bceb8e7297be8f6cdfe0965e3a5b713ea0922729
Tom Frey <tom.frey@informatik.hu-berlin.de>
2019-12-09 21:41:51 +0000

    show userapi info on Startpage

--- webapp/controller/Startpage.controller.js
+++ webapp/controller/Startpage.controller.js
@@ -1,12 +1,33 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	"use strict";
 	return Controller.extend("Mobilitaetskonto.Mobilitaetskonto.controller.Startpage", {
-		onInit: function () {},
-		
-		onNavToUmsatz: function()	{
+		onInit: function () {
+
+			var url = "/services/userapi/currentUser";
+
+			var request = $.get({
+				async: false,
+				url: url,
+				dataType: "json",
+				success: function () {
+					sap.m.MessageToast.show("success");
+				},
+				error: function () {
+					sap.m.MessageBox.alert("error");
+				}
+			});
+
+			request.done(function (data) {
+				sap.m.MessageToast.show("UserAPI:\nName: " + data.name + "\nFirstName: " + data.firstName + "\nLastName: " + data.lastName +
+					"\nEmail: " + data.email + "\nDisplayname: " + data.displayname);
+			});
+
+		},
+
+		onNavToUmsatz: function () {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("Umsatz");
-		} ,
+		},
 
 		onNavToAntrag: function () {
 			var oRouter = this.getOwnerComponent().getRouter();
