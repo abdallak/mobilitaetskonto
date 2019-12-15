9ffedf6ec0447b4c9a991c31b91611ef7870f2fb
freundma <mario.freund@informatik.hu-berlin.de>
2019-12-15 23:47:12 +0000

    Guthabenstand des aktuellen Nutzers wird jetzt auf der Umsatzkachel angezeigt

--- webapp/controller/Startpage.controller.js
+++ webapp/controller/Startpage.controller.js
@@ -6,40 +6,13 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 			var dbUserModel = sap.ui.getCore().getModel("dbUserModel").getData();
 			var userModel = sap.ui.getCore().getModel("userModel").getData();
 			
-			// var data = userModel;
-			var data = dbUserModel;
+			this.getView().byId("guthabenAnzeige").setValue(dbUserModel.GUTHABEN);
 
-			// sap.m.MessageToast.show("dbUserModel:\nMID: " + data.MID + "\nVORNAME: " + data.VORNAME + "\nName: " + data.NAME +
+			//	sap.m.MessageToast.show("dbUserModel:\nMID: " + data.MID + "\nVORNAME: " + data.VORNAME + "\nName: " + data.NAME +
 			//	"\nGuthaben: " + data.GUTHABEN);
 				
 			//sap.m.MessageToast.show("userModel:\nName: " + data.name + "\nFirstName: " + data.firstName + "\nLastName: " + data.lastName +
 			//	"\nEmail: " + data.email + "\nDisplayname: " + data.displayName);
-			
-			var params = {};
-			params.mid = dbUserModel.MID;
-			
-			var paramsString = jQuery.param(params);
-			
-			var url2 = "/MOB_GUTHABEN?" + paramsString;
-			
-			var request2 = $.get({
-				async: false,
-				url: url2,
-				dataType: "json",
-				success: function () {
-					sap.m.MessageToast.show("success");
-				},
-				error: function () {
-					sap.m.MessageToast.show("error");
-				}
-			});
-			
-			var that = this;
-			request2.done(function (data2) {
-				sap.m.MessageToast.show("Guthaben " + data2.GUTHABEN);
-				var GuthabenModel = new sap.ui.model.json.JSONModel(data2);
-				that.getView().setModel(GuthabenModel, "GuthabenModel");
-			});
 
 		},
 
