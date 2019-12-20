65e8264c64c90b2d08d791ab0366ba1bfdbc8d50
Tom Frey <tom.frey@informatik.hu-berlin.de>
2019-12-20 16:04:15 +0000

    antrag: default values

--- webapp/controller/Antrag.controller.js
+++ webapp/controller/Antrag.controller.js
@@ -1,27 +1,28 @@
 sap.ui.define(["Mobilitaetskonto/Mobilitaetskonto/controller/BaseController"], function (BaseController) {
 	"use strict";
 	return BaseController.extend("Mobilitaetskonto.Mobilitaetskonto.controller.Antrag", {
-		
+
 		onInit: function () {
 			this.getRouter().getRoute("Antrag").attachMatched(this._onRoutePatternMatched, this);
 			this.resetAntrag();
 		},
-		
+
 		_onRoutePatternMatched: function (oEvent) {
 			this.resetAntrag();
 		},
-		
+
 		resetAntrag: function () {
-			var dbUserModel = this.getGlobalModel("dbUserModel").getData();
-			var defaultAntrag = {};
-			defaultAntrag.MID = dbUserModel.MID;
-			defaultAntrag.art = "auszahlung";
-			defaultAntrag.betrag = null;
-			defaultAntrag.beschreibung = null;
+			var dbUserData = this.getGlobalModel("dbUserModel").getData();
+			var defaultAntrag = {
+				"MID": dbUserData.MID,
+				"art": "auszahlung",
+				"betrag": null,
+				"beschreibung": null
+			};
 			var oAntragModel = new sap.ui.model.json.JSONModel(defaultAntrag);
 			this.setModel(oAntragModel, "oAntragModel");
 		},
-		
+
 		antragStellen: function (oEvent) {
 			// FIXME workaround für: wenn Textfeld noch ausgewählt, also cursor blinkt, dann werden Änderungen nicht im Model übernommen
 			oEvent.getSource().focus();
@@ -39,7 +40,7 @@ sap.ui.define(["Mobilitaetskonto/Mobilitaetskonto/controller/BaseController"], f
 				that.getRouter().navTo("Umsatz");
 			});
 		},
-		
+
 		onBetragChanged: function (oEvent) {
 			var oResourceBundle = this.getResourceBundle();
 			var sValue = oEvent.getParameter("value");
@@ -52,6 +53,6 @@ sap.ui.define(["Mobilitaetskonto/Mobilitaetskonto/controller/BaseController"], f
 				oSource.setValueStateText(oResourceBundle.getText("errorEmptyBetrag"));
 			}
 		}
-		
+
 	});
 });
