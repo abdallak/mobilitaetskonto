67226660fb990dda342a4f5845f50ddc6d4f0c1f
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-01-03 15:34:52 +0000

    customformatter for antragArt

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -3,12 +3,25 @@ sap.ui.define([], function () {
 	return {
 		statusText: function (sStatus) {
 			var resourceBundle = this.getView().getModel("i18n").getResourceBundle();
-			
+
 			switch (sStatus) {
-				case "genehmigt":
-					return resourceBundle.getText("antragStatusGenehmigt");
-				default:
-					return sStatus;
+			case "genehmigt":
+				return resourceBundle.getText("antragStatusGenehmigt");
+			default:
+				return sStatus;
+			}
+		},
+
+		antragArtText: function (sAntragArt) {
+			var resourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			switch (sAntragArt) {
+			case "gutschrift":
+				return resourceBundle.getText("antragArtGutschrift");
+			case "auszahlung":
+				return resourceBundle.getText("antragArtAuszahlung");
+			default:
+				return sAntragArt;
 			}
 		}
 	};
