e1ba9a4ab550d9632035fc7f255413653a836ba6
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-01-08 20:57:15 +0000

    startpage: remove debug userdata toast

--- webapp/controller/Startpage.controller.js
+++ webapp/controller/Startpage.controller.js
@@ -8,13 +8,6 @@ sap.ui.define([
 			this.getRouter().getRoute("Startpage").attachMatched(this._onRoutePatternMatched, this);
 
 			var dbUserModel = this.getGlobalModel("dbUserModel");
-			var dbUserData = this.getGlobalModel("dbUserModel").getData();
-			// var userModel = this.getGlobalModel("userModel").getData();
-			sap.m.MessageToast.show("dbUserModel:\nMID: " + dbUserData.MID + "\nVORNAME: " + dbUserData.VORNAME + "\nName: " + dbUserData.NAME +
-				"\nGuthaben: " + dbUserData.GUTHABEN);
-			//sap.m.MessageToast.show("userModel:\nName: " + data.name + "\nFirstName: " + data.firstName + "\nLastName: " + data.lastName +
-			//	"\nEmail: " + data.email + "\nDisplayname: " + data.displayName);
-
 			this.setModel(dbUserModel, "dbUserModel");
 
 			dbUserModel.attachRequestCompleted(function (oEvent) {
