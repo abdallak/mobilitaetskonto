61826236074758d24fb45822b0a19aa23ae52f99
reuterma <martin.reuter@informatik.hu-berlin.de>
2020-01-13 20:02:23 +0000

    Formatter hinzugefuegt Laenge des Datensatz ermittelt im Table Event Label muss noch beschriftet werden dynamisch

--- webapp/controller/RequestTable.controller.js
+++ webapp/controller/RequestTable.controller.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-console, no-alert */
 sap.ui.define([
 	"Mobilitaetskonto/Mobilitaetskonto/controller/BaseController",
-	"sap/ui/core/mvc/Controller"
-], function (BaseController) {
+	"Mobilitaetskonto/Mobilitaetskonto/model/formatter"
+], function (BaseController, formatter) {
 	"use strict";
-
 	return BaseController.extend("Mobilitaetskonto.Mobilitaetskonto.controller.RequestTable", {
-
+		formatter: formatter,
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
@@ -14,39 +13,40 @@ sap.ui.define([
 		 */
 		onInit: function () {
 			
-			var requestTableModel = new sap.ui.model.json.JSONModel();
-			this.setModel(requestTableModel,"requestTableModel");
-			
-			requestTableModel.loadData("/MOB_ANTRAG_TABELLE");
-			console.log(requestTableModel);
+				var requestTableModel = new sap.ui.model.json.JSONModel();
+				requestTableModel.loadData("/MOB_ANTRAG_TABELLE");
+				this.setModel(requestTableModel, "requestTableModel");
+				
+			}
+			/**
+			 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+			 * (NOT before the first rendering! onInit() is used for that one!).
+			 * @memberOf Mobilitaetskonto.Mobilitaetskonto.view.RequestTable
+			 */
+			//	onBeforeRendering: function() {
+			//
+			//	},
+			/**
+			 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+			 * This hook is the same one that SAPUI5 controls get after being rendered.
+			 * @memberOf Mobilitaetskonto.Mobilitaetskonto.view.RequestTable
+			 */
+			//	onAfterRendering: function() {
+			//
+			//	},
+			/**
+			 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+			 * @memberOf Mobilitaetskonto.Mobilitaetskonto.view.RequestTable
+			 */
+			//	onExit: function() {
+			//
+			//	}
+			,
+		/**
+		 *@memberOf Mobilitaetskonto.Mobilitaetskonto.controller.RequestTable
+		 */
+		updateFinished: function (oEvent) {
+			var test = oEvent.getParameter("total");
 		}
-
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf Mobilitaetskonto.Mobilitaetskonto.view.RequestTable
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf Mobilitaetskonto.Mobilitaetskonto.view.RequestTable
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf Mobilitaetskonto.Mobilitaetskonto.view.RequestTable
-		 */
-		//	onExit: function() {
-		//
-		//	}
-
 	});
-
 });
