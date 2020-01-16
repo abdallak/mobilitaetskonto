fb003965c110920a4261b3c531ccd1d0bfe0db77
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-01-16 09:14:25 +0000

    no need to set model to view

--- webapp/controller/Startpage.controller.js
+++ webapp/controller/Startpage.controller.js
@@ -6,21 +6,10 @@ sap.ui.define([
 
 		onInit: function () {
 			this.getRouter().getRoute("Startpage").attachMatched(this._onRoutePatternMatched, this);
-
-			var dbUserModel = this.getGlobalModel("dbUserModel");
-			this.setModel(dbUserModel, "dbUserModel");
-
-			dbUserModel.attachRequestCompleted(function (oEvent) {
-				oEvent.getSource().refresh(true);
-			});
 		},
 
 		_onRoutePatternMatched: function (oEvent) {
 			this.updateUserModel();
-			var dbUserModel = this.getGlobalModel("dbUserModel");
-			if (dbUserModel.GUTHABEN === null){
-				this.handleEmptyModel("Aktualisierung fehlgeschlagen.");
-			}
 		},
 
 		onNavToSales: function () {
