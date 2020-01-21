4b2bc18cf6979a83708a7dc1c11015f465fa32c0
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-01-21 23:11:58 +0000

    add empty unit test for each controller

--- webapp/test/unit/AllTests.js
+++ webapp/test/unit/AllTests.js
@@ -1,5 +1,9 @@
 sap.ui.define([
-	"Mobilitaetskonto/Mobilitaetskonto/test/unit/controller/Startpage.controller"
+	"Mobilitaetskonto/Mobilitaetskonto/test/unit/controller/Startpage.controller",
+	"Mobilitaetskonto/Mobilitaetskonto/test/unit/controller/Request.controller",
+	"Mobilitaetskonto/Mobilitaetskonto/test/unit/controller/Sales.controller",
+	"Mobilitaetskonto/Mobilitaetskonto/test/unit/controller/Detail.controller",
+	"Mobilitaetskonto/Mobilitaetskonto/test/unit/controller/RequestTable.controller"
 ], function () {
 	"use strict";
 });
