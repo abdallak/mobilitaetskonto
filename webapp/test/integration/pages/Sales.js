d61ca71d6c4be3582ae538d42a89c0c9b25c57e5
freundma <mario.freund@informatik.hu-berlin.de>
2020-01-22 13:47:25 +0000

    Sales.js ergänzt

--- webapp/test/integration/pages/Sales.js
+++ webapp/test/integration/pages/Sales.js
@@ -3,6 +3,7 @@ sap.ui.define([
 ], function (Opa5) {
 	"use strict";
 	var sViewName = "Sales";
+	var sTableId = "table0";
 	Opa5.createPageObjects({
 		onTheSalesPage: {
 
@@ -19,7 +20,30 @@ sap.ui.define([
 						},
 						errorMessage: "Did not find the Sales view"
 					});
+				},
+				
+				iShouldSeeTheTable: function () {
+					return this.waitFor({
+						id: sTableId,
+						viewName: sViewName,
+						success: function () {
+							Opa5.assert.ok(true, "The Sales table is displayed");
+						},
+						errorMessage: "Did not find Sales table"
+					});
+				},
+				
+				iShouldSeeTheColumn: function (sId) {
+					return this.waitFor({
+						id: sId,
+						viewName: sViewName,
+						success: function () {
+							Opa5.assert.ok(true, "The Sales table " + sId + " is displayed");
+						},
+						errorMessage: "Did not find Sales table" + sId
+					});
 				}
+
 			}
 		}
 	});
