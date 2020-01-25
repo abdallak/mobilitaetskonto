d00f754725af66da3fa663286078c9f70c50ad16
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-01-25 11:07:59 +0000

    add unit test for kategorie destination

--- webapp/test/unit/AllTests.js
+++ webapp/test/unit/AllTests.js
@@ -3,7 +3,8 @@ sap.ui.define([
 	"Mobilitaetskonto/Mobilitaetskonto/test/unit/controller/Request.controller",
 	"Mobilitaetskonto/Mobilitaetskonto/test/unit/controller/TableSales.controller",
 	"Mobilitaetskonto/Mobilitaetskonto/test/unit/controller/DetailEmployee.controller",
-	"Mobilitaetskonto/Mobilitaetskonto/test/unit/controller/TableRequests.controller"
+	"Mobilitaetskonto/Mobilitaetskonto/test/unit/controller/TableRequests.controller",
+	"Mobilitaetskonto/Mobilitaetskonto/test/unit/model/destination/MOB_KATEGORIE"
 ], function () {
 	"use strict";
 });
