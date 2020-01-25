2b8d19458bf9f93a234524007d45ae295e54f063
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-01-25 09:14:27 +0000

    approve / decline requests in detailAD view

--- webapp/controller/DetailAD.controller.js
+++ webapp/controller/DetailAD.controller.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console, no-alert */
 sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"Mobilitaetskonto/Mobilitaetskonto/controller/BaseController",
@@ -11,23 +10,18 @@ sap.ui.define([
 
 		onInit: function () {
 			this.getRouter().getRoute("DetailAD").attachMatched(this._onRoutePatternMatched, this);
-
 			var detailADModel = new JSONModel();
 			this.setModel(detailADModel, "detailADModel");
-
 			var detailADUserModel = new JSONModel();
 			this.setModel(detailADUserModel, "detailADUserModel");
 		},
 
 		_onRoutePatternMatched: function (oEvent) {
-
 			//ANTRAGSDATEN
 			var detail = JSON.parse(oEvent.getParameter("arguments").Detail);
-
 			var detailADModel = this.getModel("detailADModel");
 			detailADModel.setData(detail);
 			//--
-
 			//USERDATEN
 			//FIXME: workaround fuer dummiedata
 			var detailADUserModel = this.getModel("detailADUserModel");
@@ -37,11 +31,7 @@ sap.ui.define([
 				firstName: "dummie-Data"
 			});
 
-
-			console.log("testUser==  " + JSON.stringify(detailADUserModel.getData()));
-
 			var oStorage = jQuery.sap.storage(jQuery.sap.storage.Type.session);
-
 			if (jQuery.isEmptyObject(detailADModel.getData())) {
 				detailADModel.setData(oStorage.get("requestTableLocalData"));
 			} else {
@@ -50,8 +40,46 @@ sap.ui.define([
 			if (detailADModel.UID === null) {
 				this.handleEmptyModel("Aktualisierung fehlgeschlagen.");
 			}
+		},
+
+		updateRequest: function (state) {
+			var dbUserData = this.getGlobalModel("dbUserModel").getData();
+			var detailADData = this.getModel("detailADModel").getData();
+
+			if (!detailADData.FEEDBACK || detailADData.FEEDBACK.trim().length === 0) {
+				this.handleEmptyModel("Feedback Feld ungültig.");
+				return;
+			}
+
+			var oRequestData = {};
+			oRequestData.amount = detailADData.BETRAG;
+			oRequestData.feedback = detailADData.FEEDBACK;
+			oRequestData.midA = dbUserData.MID;
+			oRequestData.midE = detailADData.MID;
+			oRequestData.uid = detailADData.UID;
+			oRequestData.state = state.toString();
+
+			var settings = {
+				"url": "/MOB_ANTRAG_HANDLE",
+				"method": "POST",
+				"timeout": 0,
+				"data": JSON.stringify(oRequestData)
+			};
+
+			var that = this;
+			$.ajax(settings).done(function (response) {
+				that.onNavBack();
+			}).fail(function (jqXHR, exception) {
+				that.handleEmptyModel(jqXHR.responseText + " (" + jqXHR.status + ")");
+			});
+		},
+
+		approveRequestPressed: function (oEvent) {
+			this.updateRequest(2);
+		},
+
+		rejectRequestPressed: function (oEvent) {
+			this.updateRequest(0);
 		}
-
 	});
-
 });
