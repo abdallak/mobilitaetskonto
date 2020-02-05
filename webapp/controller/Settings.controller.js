be4e67242a79500c4c87ee002f683aa305d160bc
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-02-05 16:53:19 +0000

    replace sidenavigation with toolpage

--- webapp/controller/Settings.controller.js
+++ webapp/controller/Settings.controller.js
@@ -12,6 +12,19 @@ sap.ui.define([
 
 		_onRoutePatternMatched: function (oEvent) {
 
+		},
+
+		// https://sapui5.hana.ondemand.com/#/entity/sap.tnt.ToolPage/sample/sap.tnt.sample.ToolPage/code/ToolPage.view.xml
+		onItemSelect: function (oEvent) {
+			var oItem = oEvent.getParameter("item");
+			// this.byId("pageContainer").to(this.getView().createId(oItem.getKey()));
+
+			var navContainer = this.byId("navContainer");
+			var viewId = this.getView().createId(oItem.getKey());
+
+			// console.log(oItem.getKey(), viewId);
+			// navContainer.to(this.byId(oItem.getKey()).getId());
+			navContainer.to(this.byId(oItem.getKey()));
 		}
 
 	});
