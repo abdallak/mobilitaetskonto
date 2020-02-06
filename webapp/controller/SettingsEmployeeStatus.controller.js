43970ef2400aedba86f52502e15197da0fd16120
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-02-06 21:21:14 +0000

    SettingsEmployeeStatus: inaktiv und aktiv schalten

--- webapp/controller/SettingsEmployeeStatus.controller.js
+++ webapp/controller/SettingsEmployeeStatus.controller.js
@@ -1,45 +1,70 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"Mobilitaetskonto/Mobilitaetskonto/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function (BaseController, JSONModel) {
 	"use strict";
 
-	return Controller.extend("Mobilitaetskonto.Mobilitaetskonto.controller.SettingsEmployeeStatus", {
+	return BaseController.extend("Mobilitaetskonto.Mobilitaetskonto.controller.SettingsEmployeeStatus", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf Mobilitaetskonto.Mobilitaetskonto.view.SettingsEmployeeStatus
-		 */
 		onInit: function () {
-
+			var employeeTableModel = new JSONModel();
+			this.setModel(employeeTableModel, "employeeTableModel");
+			this.getEmployeeData();
 		},
 
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf Mobilitaetskonto.Mobilitaetskonto.view.SettingsEmployeeStatus
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+		getEmployeeData: function (target) {
+			var params = {};
+			params.mid = null;
 
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf Mobilitaetskonto.Mobilitaetskonto.view.SettingsEmployeeStatus
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
+			var settings = this.prepareAjaxRequest("/MOB_MITARBEITER_GET", "GET", params);
 
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf Mobilitaetskonto.Mobilitaetskonto.view.SettingsEmployeeStatus
-		 */
-		//	onExit: function() {
-		//
-		//	}
+			var that = this;
+			$.ajax(settings).done(function (response) {
+				var employeeTableModel = that.getModel("employeeTableModel");
+				employeeTableModel.setData(response);
+			}).fail(function (jqXHR, exception) {
+				that.handleNetworkError(jqXHR);
+			});
+		},
 
+		setEmployeeStatus: function (oSelectedItems, status) {
+			var paramData = [];
+
+			for (var i = 0; i < oSelectedItems.length; i++) {
+				var context = oSelectedItems[i].getBindingContext("employeeTableModel");
+				var path = context.getPath();
+
+				var employeeStatus = {};
+				employeeStatus.status = status;
+				employeeStatus.mid = context.getProperty(path).MID;
+				paramData.push(employeeStatus);
+			}
+
+			var settings = this.prepareAjaxRequest("/MOB_STATUS_AENDERN", "POST", JSON.stringify(paramData));
+
+			var that = this;
+			$.ajax(settings).done(function (response) {
+				var employeeTableModel = that.getModel("employeeTableModel");
+				employeeTableModel.setData(response);
+			}).fail(function (jqXHR, exception) {
+				that.handleNetworkError(jqXHR);
+			});
+		},
+
+		onAktivPressed: function (oEvent) {
+			var oList = this.byId("list0");
+			var oSelectedItems = oList.getSelectedItems();
+			this.setEmployeeStatus(oSelectedItems, true);
+		},
+
+		onInaktivPressed: function (oEvent) {
+			var oList = this.byId("list0");
+			var oSelectedItems = oList.getSelectedItems();
+			this.setEmployeeStatus(oSelectedItems, false);
+		},
+
+		onDeletePressed: function (oEvent) {
+			sap.m.MessageToast.show("Leider noch nicht implementiert.");
+		}
 	});
-
 });
