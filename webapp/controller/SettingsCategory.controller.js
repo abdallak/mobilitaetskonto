c6117beac5a0ca1ad186c1bb6d789e8811eccaba
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-02-06 16:42:02 +0000

    SettingsCategory: add edit and delete dialogs

--- webapp/controller/SettingsCategory.controller.js
+++ webapp/controller/SettingsCategory.controller.js
@@ -5,12 +5,16 @@ sap.ui.define([
 	"sap/ui/layout/VerticalLayout",
 	"sap/m/ButtonType",
 	"sap/m/Input",
+	"sap/m/Label",
+	"sap/m/Text",
 	"sap/m/MessageToast",
 	"sap/m/Button",
 	"sap/ui/model/json/JSONModel"
-], function (BaseController, Dialog, HorizontalLayout, VerticalLayout, ButtonType, Input, MessageToast, Button, JSONModel) {
+], function (BaseController, Dialog, HorizontalLayout, VerticalLayout, ButtonType, Input, Label, Text, MessageToast, Button, JSONModel) {
 	"use strict";
+
 	return BaseController.extend("Mobilitaetskonto.Mobilitaetskonto.controller.SettingsCategory", {
+
 		onInit: function () {
 			// FIXME: Funktioniert für den NavContainer nicht
 			// this.getRouter().getRoute("SettingsCategory").attachMatched(this._onRoutePatternMatched, this);
@@ -21,8 +25,20 @@ sap.ui.define([
 
 		_onRoutePatternMatched: function (oEvent) {},
 
-		changeCategory: function (type) {
+		changeCategory: function (type, kid) {
 			// TODO network request
+
+			switch (type) {
+			case "add":
+				break;
+			case "delete":
+				break;
+			case "edit":
+				break;
+			default:
+				break;
+			}
+
 		},
 
 		fetchCategories: function () {
@@ -38,10 +54,68 @@ sap.ui.define([
 
 		onDeletePressed: function (oEvent) {
 			var kid = oEvent.getSource().data("KID");
+
+			var oDialog = new Dialog({
+				title: "Bestätigen",
+				type: "Message",
+				content: new Label({
+					text: "Sind Sie sich sicher, dass Sie die Kategorie löschen möchten?"
+				}),
+				beginButton: new Button({
+					type: ButtonType.Emphasized,
+					text: "Löschen",
+					press: function () {
+						this.changeCategory("delete", null, kid);
+						oDialog.close();
+					}
+				}),
+				endButton: new Button({
+					text: "Abbrechen",
+					press: function () {
+						oDialog.close();
+					}
+				}),
+				afterClose: function () {
+					oDialog.destroy();
+				}
+			});
+
+			oDialog.open();
 		},
 
 		onEditPressed: function (oEvent) {
 			var kid = oEvent.getSource().data("KID");
+
+			var oDialog = new Dialog({
+				title: "Kategorie Namen ändern",
+				type: "Message",
+				content: [
+					new Input("newCategoryNameInput", {
+						width: "100%",
+						placeholder: "Neuer Name der Kategorie"
+					})
+				],
+				beginButton: new Button({
+					type: ButtonType.Emphasized,
+					text: "Ändern",
+					press: function () {
+						var sText = sap.ui.getCore().byId("newCategoryNameInput").getValue();
+						this.changeCategory("edit", sText, kid);
+						oDialog.close();
+					}
+				}),
+				endButton: new Button({
+					text: "Abbrechen",
+					press: function () {
+						oDialog.close();
+					}
+				}),
+				afterClose: function () {
+					oDialog.destroy();
+				}
+			});
+
+			oDialog.open();
 		},
 
 		onAddPressed: function (oEvent) {
