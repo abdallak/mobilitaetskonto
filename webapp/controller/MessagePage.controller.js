c8433056a4faa2551e20f9d495ad6f0d7bd9a536
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-02-12 19:10:36 +0000

    messagePage: show error data

--- webapp/controller/MessagePage.controller.js
+++ webapp/controller/MessagePage.controller.js
@@ -1,17 +1,23 @@
 sap.ui.define([
-	"Mobilitaetskonto/Mobilitaetskonto/controller/BaseController"
-], function (BaseController) {
+	"Mobilitaetskonto/Mobilitaetskonto/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function (BaseController, JSONModel) {
 	"use strict";
 
 	return BaseController.extend("Mobilitaetskonto.Mobilitaetskonto.controller.MessagePage", {
 
 		onInit: function () {
+			var errorPageModel = new JSONModel();
+			this.setModel(errorPageModel, "errorPageModel");
+
 			this.getRouter().getRoute("MessagePage").attachMatched(this._onRoutePatternMatched, this);
 		},
 
 		_onRoutePatternMatched: function (oEvent) {
 			var errorPageModel = this.getModel("errorPageModel");
-			errorPageModel.setData(oEvent.getParameter("arguments"));
+			var oErrorData = JSON.parse(oEvent.getParameter("arguments").error);
+			errorPageModel.setData(oErrorData);
+			console.log(oErrorData);
 		}
 
 	});
