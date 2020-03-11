c15f5a40f212eaf70d3639f75d01c38f19d3d27a
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-03-11 13:38:36 +0000

    fix verwalter inactive case

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -86,10 +86,11 @@ sap.ui.define([
 
 					var roleModel = component.getModel("roleModel");
 					var roleData = roleModel.getData();
-					roleData.verwalter = response.FREIGABEWERT > 0;
+
 					// FIXME: Keine Ahnung warum hier der Boolean der String "TRUE" ist statt 1 oder true..
 					// In XSJS Service gibt es scheinbar kein BOOLEAN Typ und wird auch nicht als Integer erkannt.
 					roleData.mitarbeiter = response.AKTIV === "TRUE";
+					roleData.verwalter = response.FREIGABEWERT > 0 && roleData.mitarbeiter;
 
 					roleModel.refresh(true);
 
